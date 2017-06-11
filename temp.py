1fe5627d2ca71b9e454e2f9d8fe83fe68bb7c8a6
vpatov <vasiapatov@gmail.com>
2017-06-11 12:04:06 -0400

    if youre gonna accuse me

--- temp.py
+++
@@ -1,7 +0,0 @@
-import os,sys,random,subprocess
-
-dir = 'C:\\Users\\Vasia\\Documents\\PycharmProjects\\'
-p = subprocess.Popen(['dir'],shell=True,cwd=dir)
-p.wait()
-p = subprocess.Popen(['dir'],shell=True,cwd=dir)
-p.wait()
